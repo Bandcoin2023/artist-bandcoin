2ff91615509a12879bc8203d9c4cd69e4bf23537
Foysal Sheikh <support@bandcoin.io>
2025-02-16 14:55:38 +0600

    redeploy

--- src/components/layout/root/AdminLayout.tsx
+++ src/components/layout/root/AdminLayout.tsx
@@ -74,7 +74,6 @@ export default function AdminLayout({
                             </div>
                         )
                 }
-
             </motion.div>
             <AnimatePresence>
                 <motion.div
