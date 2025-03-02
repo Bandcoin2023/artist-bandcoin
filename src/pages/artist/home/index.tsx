c20ceb99920bd9dd4f86682204605e43f3edce59
Foysal Sheikh <support@bandcoin.io>
2025-03-02 15:18:47 +0600

    layout fix

--- src/pages/artist/home/index.tsx
+++ src/pages/artist/home/index.tsx
@@ -4,7 +4,17 @@ const ArtistHome = () => {
     return (
         <CreatorLayout>
             <div>
-                <h1>Artist Home</h1>
+
+                {
+                    Array(10).fill(0).map((_, index) => (
+                        <div key={index} className="p-4 border-b border-gray-200">
+                            <h1>Artist Home</h1>
+                            <h1>Artist Home</h1>
+                            <h1>Artist Home</h1>
+                            <h1>Artist Home</h1>
+                        </div>
+                    ))
+                }
             </div>
         </CreatorLayout>
     )
